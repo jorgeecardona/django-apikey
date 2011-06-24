0b3be65cb54f57dd3f4ac153c84607d241c96eff
Jorge Eduardo Cardona <jorgeecardona@gmail.com>
2011-06-24 17:20:43 -0500

    Change version

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
+from os import path
+BASE_DIR = path.abspath(path.dirname(__file__))
+
 
 setup(
     name='django-apikey',
     description='Simple Key authentication for Django.'\
     'Based on  github.com/scoursen/django-apikey',
-    version='0.1.6',
-    long_description=open('README.rst').read(),
+    version='0.1.7',
+    long_description=open(path.join(BASE_DIR, 'README.rst')).read(),
     url='https://www.github.com/jorgeecardona/django-apikey',
     packages=find_packages(),
     author='Jorge Eduardo Cardona',
